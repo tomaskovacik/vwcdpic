54330e54fe46eb3b73c5c87b5400a1ca1b73b767
Tomas Kovacik <tomas.kovacik@yahoo.com>
2014-07-02 18:15:25 +0200

    del obsolete files, update Readm

--- genbookmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pikovina=""
-lines=`cat ${1}|wc -l`
-for line in `seq 1 1 ${lines}`;
-do
-	echo "bookmarks."${line}"="`head -n ${line} ${1}|tail -n 1|sed 's/;/'${pikovina}'/g'`
-done
-
