8862e3701e2028bc2f0b4eeca4567df6171db71a
Tomas Kovacik <tomas.kovacik@yahoo.com>
2014-07-03 12:25:29 +0200

    dafav.sh update

--- dafav.sh
+++ dafav.sh
@@ -37,21 +37,28 @@ stty echo
 fi
 
 curl -s -c $cookie -d password=$password -d username=$username -d reusetoken=1 https://www.deviantart.com/users/login
-favs=`curl -s -b $cookie http://$username.deviantart.com/|grep \<\/strong\>Favourites|sed 's/.*<strong>\([0-9 ]*\)<\/strong>.*/\1/'|sed 's/\ //g'`
+favs=`curl -s -b $cookie http://$username.deviantart.com/|grep \<\/strong\>Favourites|sed 's/.*<strong>\([,0-9 ]*\)<\/strong>.*/\1/'|sed 's/\,//g'|sed 's/\ //g'`
+echo $favs favourites.
 for i in `seq 0 24 $favs`
 do
 arts=`curl -s -b $cookie "http://backend.deviantart.com/rss.xml?q=favby%3A$username&type=deviation&offset=$i"|grep \<link\>|sed 's/.*<link>\(.*\)<\/link>.*/\1/'|grep \/art\/`
-
+count=0
 for art in $arts
 do
-picurl=`curl -s -b $cookie $art |grep class=\"fullview|sed 's/.*src="\(.*\)".*/\1/'|cut -d\" -f1`
+count=`expr $count + 1`
+echo [ $count ] =========================================================
+echo ART: $art
+#picurl=`curl -s -b $cookie $art |grep class=\"fullview|sed 's/.*src="\(.*\)".*/\1/'|cut -d\" -f1`
+picurl=`curl -s -b $cookie $art |grep -B 10 class=\"fullview\ smshadow\"|tr -d '\n\r\t '|sed 's/.*collect_rid="[0-9:]*"src="\(.*\)"width="[0-9]*"height="[0-9]*"alt=""class="fullviewsmshadow">.*/\1/'`
 filename=`echo $picurl|sed 's/.*\/\(.*\)\..../\1/'`
-echo $filename
 ext=`echo $picurl|sed 's/.*\.\([jpng]*\)/\1/'`
 file=$outputdir/$filename.$ext
 if [ ! -e $file ]
 then
-wget -v $picurl -P $outputdir
+echo -n "Downloading: "$picurl" to "$outputdir" "
+wget -q $picurl -P $outputdir && echo [ OK ]
+else
+echo Already downloaded. Skiped.
 fi
 done
 done
